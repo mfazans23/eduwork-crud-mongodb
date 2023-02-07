a4bba42d42906f775e68b715c9064a777d7547c6
mfazans23 <92628882+mfazans23@users.noreply.github.com>
2023-02-07 20:17:37 +0700

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 
 require('dotenv').config()
 
-const connection =
-  'mongodb+srv://mfazans23:mfazans23@cluster0.vex5oxc.mongodb.net/eduwork-native?retryWrites=true&w=majority'
+const connection = process.env.MONGO_URI
 
 mongoose.set('strictQuery', false)
 
